772a751d7e653bd3ef119f0cdbd93534a7766813
marvin <marvin@vinova.com.sg>
2025-09-14 15:45:00 +0700

    fix stupid Sonys bugs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -204,7 +204,7 @@ const HomePage: React.FC = () => {
           <div className="mt-6">
             <DashboardPage />
           </div>
-        )} */}
+        )} 
 
         {canViewChart && (
           <div className="bg-white p-6 rounded-lg border border-gray-200 mt-6">
